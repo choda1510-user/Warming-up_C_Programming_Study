531a1f4e022149a8775f8257dd2dc25f80973786
choda1510-user <choda1510@gmail.com>
2025-05-19 23:18:12 +0900

    405 11 code wrote.

--- 402-405/11/main.c
+++ 402-405/11/main.c
@@ -11,12 +11,39 @@ typedef struct _song
     char signer[64];
     song_jangr jangr;
 } song;
-
+char* song_jangr_string(song_jangr j);
+void print_all(song *ptr, int length, int size);
 
 int main(int argc, char *argv[])
 {
-    song list[10] = { { "song1", "singer1", ballad }, { "song2", "singer2", ballad }, { "song3", "singer3", hip-hop } };
-
+    song list[10] = { { "song1", "singer1", ballad }, { "song2", "singer2", ballad }, { "song3", "singer3", hip_hop } };
+    print_all(list, sizeof(list)/sizeof(song), 3);
     return 0;
 }
 
+char* song_jangr_string(song_jangr j)
+{
+    switch(j)
+    {
+        case ballad:
+            return "ballad";
+        case hip_hop:
+            return "hip_hop";
+        case soul:
+            return "soul";
+        case dance:
+            return "dance";
+        case pop:
+            return "pop";
+    }
+    return "empty";
+}
+void print_all(song *ptr, int length, int size)
+{
+    int i = 0;
+    printf("%-10s %-10s %-10s\n", "title", "artist", "jangr");
+    for(i = 0; i < size; i++)
+    {
+        printf("%-10s %-10s %-10s\n", ptr[i].name, ptr[i].signer, song_jangr_string(ptr[i].jangr));
+    }
+}
